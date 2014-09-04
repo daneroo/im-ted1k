4dc1ad178cb04a92a288641569dc5f02e0b42079
Daniel Lauzon <daniel.lauzon@gmail.com>
2014-09-04 04:35:09 -0400

    fix fig monnitor entry command, and format monitor output

--- monitor.sh
+++ monitor.sh
@@ -3,12 +3,12 @@
 MYSQL_PORT_3306_TCP_ADDR='172.17.42.1'
 MYSQL_PORT_3306_TCP_PORT='3306'
 MYSQL_ENV_MYSQL_DATABASE='ted'
-while true; do   
-done
 while true; do
+  date  +"  %Y-%m-%d %H:%M:%S     %Z monitoring (localtime)"
   for i in watt_day watt_hour watt_minute watt_tensec watt ; do
-    echo `mysql -h"$MYSQL_PORT_3306_TCP_ADDR" -P"$MYSQL_PORT_3306_TCP_PORT" $MYSQL_ENV_MYSQL_DATABASE -N -B -e "select now(),'--',stamp,watt from $i order by stamp desc limit 1"` $i;
+    echo `mysql -h"$MYSQL_PORT_3306_TCP_ADDR" -P"$MYSQL_PORT_3306_TCP_PORT" $MYSQL_ENV_MYSQL_DATABASE -N -B -e "select '-',stamp,watt from $i order by stamp desc limit 1"` UTC $i;
   done;
+  date -u +"- %Y-%m-%d %H:%M:%S     UTC now"
   sleep 9; 
   echo;
 done
