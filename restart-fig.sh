41e4b49ae67996ed8482325facd28eb439640970
Daniel Lauzon <daniel.lauzon@gmail.com>
2014-12-04 01:49:12 -0500

    add script for cron

---
+++ restart-fig.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# should be cron'd with
+# 9 */4 * * *  cd /home/daniel/im-ted1k; ./restart.sh >>figup.log 2>&1
+
+date -u +'%Y-%m-%dT%H:%M:%SZ Restarting fig (up -d)'
+
+/usr/local/bin/fig up -d
+
+date -u +'%Y-%m-%dT%H:%M:%SZ Done'
