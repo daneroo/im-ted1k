53431061fae1d7adcedaf05ec7d392646c711e38
Daniel Lauzon <daniel.lauzon@gmail.com>
2015-08-28 02:20:34 -0400

    rename and use compose (not fig)

--- restart-compose.sh
+++ restart-compose.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 # should be cron'd with
-# 9 */4 * * *  cd /home/daniel/im-ted1k; ./restart-fig.sh >>restart-fig.log 2>&1
+# 9 */4 * * *  cd /home/daniel/im-ted1k; ./restart-compose.sh >>restart-compose.log 2>&1
 date -u +'%Y-%m-%dT%H:%M:%SZ Restarting fig (up -d)'
 
-/usr/local/bin/fig up -d
+/usr/local/bin/docker-compose up -d
 
 date -u +'%Y-%m-%dT%H:%M:%SZ Done'
