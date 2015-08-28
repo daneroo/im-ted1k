8bf2fb7dbe6a1e2460e55cd67301c869456770e8
Daniel Lauzon <daniel.lauzon@gmail.com>
2015-08-28 02:03:33 -0400

    rename  cron log

--- restart-fig.sh
+++ restart-fig.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 
 # should be cron'd with
-# 9 */4 * * *  cd /home/daniel/im-ted1k; ./restart.sh >>figup.log 2>&1
-
+# 9 */4 * * *  cd /home/daniel/im-ted1k; ./restart-fig.sh >>restart-fig.log 2>&1
 date -u +'%Y-%m-%dT%H:%M:%SZ Restarting fig (up -d)'
 
 /usr/local/bin/fig up -d
