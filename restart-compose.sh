617381561c7822ed9d177b24854730aaf8d6385e
Daniel Lauzon <daniel.lauzon@gmail.com>
2015-08-28 22:06:51 -0400

    Logging message in restart script

--- restart-compose.sh
+++ restart-compose.sh
@@ -2,7 +2,7 @@
 
 # should be cron'd with
 # 9 */4 * * *  cd /home/daniel/im-ted1k; ./restart-compose.sh >>restart-compose.log 2>&1
-date -u +'%Y-%m-%dT%H:%M:%SZ Restarting fig (up -d)'
+date -u +'%Y-%m-%dT%H:%M:%SZ Restarting docker-compose (up -d)'
 
 /usr/local/bin/docker-compose up -d
 
