61331f9e716204be7133a5a5255c50844d263173
Daniel Lauzon <daniel.lauzon@gmail.com>
2015-11-30 15:52:26 -0500

    First try:

--- go/ted1k.go
+++ go/ted1k.go
@@ -7,21 +7,22 @@ import (
 )
 
 func main() {
-	c := &serial.Config{Name: "COM45", Baud: 115200}
+	// omitted ReadTimeout: e.g.: time.Millisecond * 500
+	c := &serial.Config{Name: "/hostdev/ttyUSB0", Baud: 19200}
 	s, err := serial.OpenPort(c)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	n, err := s.Write([]byte("test"))
+	n, err := s.Write([]byte("\xAA"))
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	buf := make([]byte, 128)
+	buf := make([]byte, 4096)
 	n, err = s.Read(buf)
 	if err != nil {
 		log.Fatal(err)
 	}
-	log.Printf("%q", buf[:n])
+	log.Printf("n:%d buf[:n]:%q", n, buf[:n])
 }
