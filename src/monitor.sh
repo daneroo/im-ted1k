ab512dab19a997c89d83985403623566f85c3aa4
Daniel Lauzon <daniel.lauzon@gmail.com>
2016-09-17 22:42:53 -0400

    realign monitor and add per day/hour counts

--- src/monitor.sh
+++ src/monitor.sh
@@ -4,17 +4,24 @@
 MYSQL_PORT_3306_TCP_ADDR='teddb'
 MYSQL_PORT_3306_TCP_PORT='3306'
 MYSQL_ENV_MYSQL_DATABASE='ted'
-LOOP_PERIOD_SECONDS=30
+LOOP_PERIOD_SECONDS=60
 
 unset sleepPID
 trap 'echo TERMinated; kill $sleepPID; exit' TERM
 
+MYCMD="mysql -h${MYSQL_PORT_3306_TCP_ADDR} -P${MYSQL_PORT_3306_TCP_PORT} $MYSQL_ENV_MYSQL_DATABASE"
 while true; do
-  date  +"  %Y-%m-%d %H:%M:%S     %Z monitoring (localtime)"
   for i in watt_day watt_hour watt_minute watt_tensec watt ; do
-    echo `mysql -h"$MYSQL_PORT_3306_TCP_ADDR" -P"$MYSQL_PORT_3306_TCP_PORT" $MYSQL_ENV_MYSQL_DATABASE -N -B -e "select '-',stamp,watt from $i order by stamp desc limit 1"` UTC $i;
+    STMP_WTT=`${MYCMD} -N -B -e "select watt,watt*24/1000,concat(stamp,' UTC') from $i order by stamp desc limit 1"`
+    printf "  latest %12s:  %s\n"  $i "${STMP_WTT}";
   done;
-  date -u +"- %Y-%m-%d %H:%M:%S     UTC now"
+  printf "  NOW %53s UTC\n" "`date -u +'%Y-%m-%d %H:%M:%S'`"
+  printf "  NOW %57s\n" "`date +'%Y-%m-%d %H:%M:%S %Z'`"
+  
+  # Per day counts
+  ${MYCMD} -e 'select left(stamp,10) as perday,count(*) from watt where stamp>DATE_SUB(NOW(), INTERVAL 7 day) group by perday';
+  # Per hour counts
+  ${MYCMD} -e 'select concat(left(stamp,13),":00") as perhour,count(*) from watt where stamp>DATE_SUB(NOW(), INTERVAL 12 hour) group by perhour';
 
   sleep ${LOOP_PERIOD_SECONDS} & sleepPID=$!; wait
 done
