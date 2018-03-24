be841118e96bcb0af9acbf6548e13bc7f9c1d948
Daniel Lauzon <daniel.lauzon@irdeto.com>
2018-03-23 23:24:18 -0400

    Add hellogopher Makefile (orig)

---
+++ go/cmd/capture/main.go
@@ -0,0 +1,12 @@
+package main // import "github.com/daneroo/im-ted-1k/go"
+
+import (
+	"log"
+
+	"github.com/daneroo/im-ted-1k/go/ted1k"
+)
+
+func main() {
+	log.Println("Calling TED")
+	ted1k.Doit()
+}
